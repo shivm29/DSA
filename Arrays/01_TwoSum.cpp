3780acef733f2b5d826acd2ca634dac10dbeeb3d
Shivam Puri <puri46394@gmail.com>
2023-09-11 20:42:26 +0530

    two sum

---
+++ Arrays/01_TwoSum.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/two-sum
+
+class Solution {
+public:
+vector<int> twoSum(vector<int> &nums, int target){
+
+  unordered_map<int, int> map;
+  int complement;
+
+  for(int i = 0; i < nums.size(); i++) {
+    complement = target - nums[i];
+
+    if(map.count(complement) > 0) {
+      return {map[complement], i};
+    }
+
+    map[nums[i]] = i;
+
+  }
+
+  return {};
+}
+};
