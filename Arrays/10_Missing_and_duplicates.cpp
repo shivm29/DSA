54210855c7d4947ce0463ba2cf2bf00887ca15c7
Shivam Puri <puri46394@gmail.com>
2023-09-21 09:42:34 +0530

    added new

--- Arrays/10_Missing_and_duplicates.cpp
+++ Arrays/10_Missing_and_duplicates.cpp
@@ -4,44 +4,32 @@ using namespace std;
 
 // Think about this "visit and mark" concept when constraints are such that array has elements from 1 to N
 // TC : O(N) SC : O(1)
-// approach I by codehelp
-// cannot count duplicate element in {1,2,2,3,4,5,6,7}
+// approach I by gfg
 void missingAndRepeating(vector<int> &v)
 {
-    vector<int> ans;
+    cout << "Repeated numbers is/are : " << endl;
 
     for (int i = 0; i < v.size(); i++)
     {
-        int nextIndex = abs(v[i]) - 1;
-        if (v[nextIndex] > 0)
-            v[nextIndex] *= (-1);
+        if (v[abs(v[i]) - 1] > 0)
+            v[abs(v[i]) - 1] *= -1;
+        else
+            cout << abs(v[i]) << " ";
     }
 
+    cout << "\nMissing numbers is/are : " << endl;
+
     for (int i = 0; i < v.size(); i++)
     {
-
         if (v[i] > 0)
-        {
-            ans.push_back(i + 1);
-        };
-    }
-    cout << "missing : " << endl;
-    for (int i = 0; i < ans.size(); i++)
-    {
-        cout << ans[i] << " ";
-    }
-
-    cout << "\nrepeating : " << endl;
-    for (int i = 0; i < ans.size(); i++)
-    {
-        cout << v[ans[i] - 1] << " ";
+            cout << (i + 1) << " ";
     }
 }
 
 int main()
 {
 
-    vector<int> v = {1, 3, 5, 3, 4};
+    vector<int> v = {7, 3, 4, 5, 5, 6, 5};
     missingAndRepeating(v);
 
     return 0;
